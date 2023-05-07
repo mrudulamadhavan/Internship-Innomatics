e47f0e684ff65e4866925dc99eec039a893bb7f8
Mrudula Madhavan <122630243+mrudulamadhavan@users.noreply.github.com>
2023-05-07 17:55:03 +0530

    Update Home.py

--- m_asgmt_prod/Home.py
+++ m_asgmt_prod/Home.py
@@ -7,7 +7,7 @@ st.header(":blue[Diamond Price Predictor Application ]")
 
 
 #Adding Image
-FILE_DIR = os.path.dirname(os.path.abspath('C://Users//Mrudula Madhavan//Desktop//Innomatics//MLOps//m_asgmt_prod//Home.py'))
+FILE_DIR = os.path.dirname(os.path.abspath('m_asgmt_prod/Home.py'))
 dir_of_interest = os.path.join(FILE_DIR, "resources")
 IMAGE_PATH = os.path.join(dir_of_interest, "images")
 IMAGE_PATH1 = os.path.join(IMAGE_PATH, "dia2.jpg")
